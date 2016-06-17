bf33267343117df1195042ddf8397826af5ea8e0
Mukesh Kumar <M1036022@mindtree.com>
2016-06-17 17:51:33 +0530

    First Commit Setup

---
+++ app/app.component.ts
@@ -0,0 +1,19 @@
+import {Component} from "@angular/core"
+import {MD_BUTTON_DIRECTIVES} from "@angular2-material/button"
+//import {MD_ICON_DIRECTIVES} from"@angular2-material/icon"
+@Component({
+    selector:"my-app",
+    template:`<h1>Hello Angular 2</h1>
+        <button md-button color="primary">Flat</button>
+        <button md-raised-button class="btn1">Raised</button>
+        <button md-icon-button>
+            <md-icon class="md-24">Favourite</md-icon>
+        </button>
+    
+    `,
+    directives: [MD_BUTTON_DIRECTIVES]
+})
+
+export class AppComponent{
+
+}
---
+++ app/components/Movies/movies.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector:"movies",
+    templateUrl:"moviesView.html"
+})
+
+export class MoviesComponent{
+    
+}
